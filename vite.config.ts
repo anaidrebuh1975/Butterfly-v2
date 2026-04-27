f7713ee01ae5323ecc0f30bbcddf52696a5e4c12
Diana <anaidrebuh@gmail.com>
2026-04-27 10:58:47 +0200

    fix icons + base FINAL

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/Butterfly-v2/",
     plugins: [
       react(), 
       tailwindcss(),
